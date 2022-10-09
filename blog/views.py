ccb91ba36ad659bae1e964995e59b8fc1e604542
MichaelChildress06 <https://github.com/MichaelChildress06/design_interface_project_main.git>
2022-10-09 02:11:22 -0500

    added about, base, and home templates for template inheritance. updated views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
+
+posts = [
+    {
+        'author': 'Michael',
+        'title': 'Post 1',
+        'content': 'content',
+        'date_posted': 'Aug 234324, 345453'
+    },
+    {
+        'author': 'Mich',
+        'title': 'Post 2',
+        'content': 'content2',
+        'date_posted': 'Aufgdfg34324, 345453'
+    },
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
 
